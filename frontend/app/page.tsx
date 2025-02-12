bfeb5ec86e6c13aa744877793f20ac719c7b92e8
Hwang <jkimg9170@naver.com>
2025-02-12 19:38:03 +0900

    수정

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import "./globals.css"; // CSS 파일 적용
 
 export default function Home() {
   return (
@@ -9,10 +8,25 @@ export default function Home() {
         노인과 요양사를 연결하는 믿을 수 있는 서비스, <br />
         편리하게 요양 서비스를 찾고 제공하세요.
       </p>
+
       <div className="btn-group">
-        <Link href="/signup" className="btn btn-signup">회원가입</Link>
-        <Link href="/login" className="btn btn-login">로그인</Link>
+        {/* 센터 관계자 선택 */}
+        <Link href="/signup/center" className="option-card">
+          <p className="option-description">어르신을 돌봐줄 보호사님을 찾으세요?</p>
+          <p className="option-title">센터 관계자로 시작하기 →</p>
+        </Link>
+
+        {/* 요양 보호사 선택 */}
+        <Link href="/signup/caregiver" className="option-card">
+          <p className="option-description">일자리를 찾으세요?</p>
+          <p className="option-title">요양 보호사로 시작하기 →</p>
+        </Link>
       </div>
+
+      {/* 로그인 버튼 */}
+      <Link href="/login" className="btn btn-login">
+        바로 로그인 하기
+      </Link>
     </div>
   );
 }
